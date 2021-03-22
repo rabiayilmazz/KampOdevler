836d2129b791b3d60522ebcbdbc91074a159e89c
rabia yılmaz <rabiarabiabbb@gmail.com>
2021-03-22 16:24:36 +0300

    ..

---
+++ 3. gün/ClassMetodDemo/ClassMetodDemo/Musteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetodDemo
+{
+    public class Musteri
+    {
+        public string Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Mail { get; set; }
+    }
+}
---
+++ 3. gün/ClassMetodDemo/ClassMetodDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetodDemo
+{
+    public class MusteriManager
+    {
+        void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "Musteri eklendi");
+        }
+       void Remove(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "Musteri silindi");
+        }
+        void Update(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "musteri güncellendi");
+        }
+
+    }
+}
